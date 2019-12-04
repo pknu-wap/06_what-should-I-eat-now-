03f43cd8eda2ac9ff46e08684703e60cc4570696
jeongname <dlqkqhdi3259@gmail.com>
2019-12-04 15:39:05 +0900

    1. 메뉴에 해당하는 식당 지도, 위치 띄우기 2. 메뉴에 해당하는 사진 띄우기

---
+++ whatshouldieatnow/jss/map1.js
@@ -0,0 +1,98 @@
+var goukbab=[
+    [35.1370719,129.1020758,'형제국밥','<li class="title" style="width:100%;">형제국밥</li>'+'<li class="content">▲ 돼지국밥 6000￦<br>▲ 내장국밥 6000￦<br>▲ 섞어국밥 6000￦<br>▲ 순대국밥 6000￦<br>▲ 불꽃국밥 6000￦<br>▲ 우동국밥 6000￦<br>▲ 간장수육 大 32000￦ 小 22000￦<br>▲ 형제수육 32000￦ 小 22000￦<br><br><br>◆ 부산광역시 남구 대연3동 용소로13번길 29-1<br>◆ 051-625-1014</li>'],
+    [35.1381588,129.1037925,'영진돼지국밥','<li class="title" style="width:100%;">영진돼지국밥</li>'+'<li class="content">▲ 따로국밥 7500￦<br>▲ 섞어국밥 7500￦<br>▲ 순대국밥 7500￦<br>▲ 만두국밥 7500￦<br>▲ 내장국밥 7500￦<br>▲ 항정수육/모듬수육 소 30000￦<br>▲ 만두 1접시 5000￦<br>▲ 순대 1접시 8000￦<br>▲ 수육 1인분 10000￦<br><br><br>◆ 부산광역시 대연동 수영로346번길 18<br>◆ 051-621-0045</li>'],
+    [35.1359906,129.1014348,'합천돼지국밥','<li class="title" style="width:100%;">합천돼지국밥</li>'+'<li class="content">▲ 따로국밥 8000￦<br>▲ 섞어따로국밥 8000￦<br>▲ 내장따로국밥 8000￦<br>▲ 순대따로국밥 8000￦<br>▲ 모듬따로국밥 8000￦<br>▲ 수육백반 9000￦<br>▲ 수육 大 30000￦ 小 25000￦<br>▲ 모듬수육 大 30000￦ 小 25000￦<br><br><br>◆ 부산 남구 용소로19번길 13<br>◆ 051-637-8770</li>'],
+    [35.1360028,129.1000579,'통큰할매순대국','<li class="title" style="width:100%;">통큰할매순대국</li>'+'<li class="content">▲ 토종순대국 7000￦<br>▲ 순대국정식 9000￦<br>▲ 순계탕(삼계탕+순대) 8000￦<br>▲ 육순탕 8000￦<br>▲ 뼈해장국 8000￦<br>▲ 통큰순대철판볶음 15000￦<br>▲ 통큰순대소곱창전골 19000￦<br><br><br>◆ 부산광역시 대연동 수영로346번길 18<br>◆ 051-621-0045</li>'],
+    [35.1361101,129.0993896,'수라돼지국밥','<li class="title" style="width:100%;">수라돼지국밥</li>'+'<li class="content">▲ 돼지따로국밥 7000￦<br>▲ 순대따로국밥 7500￦<br>▲ 내장따로국밥 7500￦<br>▲ 섞어따로국밥 7500￦<br>▲ 해장돼지국밥 7500￦<br>▲ 수육국밥 8500￦<br>▲ 수육백반 10000￦<br>▲ 수육 大 30000￦ 小 25000￦<br><br><br>◆ 부산 남구 용소로8번길 21 행운빌딩<br>◆ 051-627-5557</li>'],
+    [35.1361029,129.0976813,'돈당돼지국밥','<li class="title" style="width:100%;">돈당돼지국밥</li>'+'<li class="content">▲ 돼지따로국밥 7000￦<br>▲ 순대국밥 7000￦<br>▲ 내장국밥 7000￦<br>▲ 섞어국밥 7000￦<br>▲ 마늘국밥 7000￦<br>▲ 치즈국밥 7000￦<br>▲ 수육백반 9000￦<br>▲ 수육 大 25000￦ 小 15000￦<br><br><br>◆ 부산광역시 남구 대연동 수영로 286<br>◆ 051-868-9698</li>'],
+    [35.1385322,129.1046955,'알천순대','<li class="title" style="width:100%;">알천순대</li>'+'<li class="content">▲ 순대국밥 8000￦<br>▲ 순대만국밥 8000￦<br>▲ 내장국밥 8000￦<br>▲ 섞어국밥 8000￦<br>▲ 순대1접시 12000￦<br>▲ 돼지국밥 7000￦<br>▲ 순대곱창전골 12000￦<br>▲ 순대만전골 12000￦ 小 15000￦<br><br><br>◆ 부산 남구 용소로7번길 88<br>◆ 051-612-5952</li>']
+];
+var pizza=[
+    [35.136449,129.1002788,'미스터피자','<li class="title" style="width:100%;">미스터피자</li>'+'<li class="content">▲ 아모르파티R 노엣지 28500￦<br>▲ 아모르파티 쉬름프R 노엣지 28500￦<br>▲ 씨푸드아일랜드R 골드 29500￦<br>▲ 하프앤하프R 골드 27900￦<br>▲ 쉬림프골드R 27900￦<br>▲ 포테이토골드R 26900￦<br><br><br>◆ 부산광역시 남구 대연3동 50-30<br>◆ 051-623-1082</li>'],
+    [35.13756,129.1034733,'원플러스피자','<li class="title" style="width:100%;">원플러스피자</li>'+'<li class="content">▲ 단호박골드 25000￦<br>▲ 앙치즈바이트 22900▲ ￦<br>▲ 크라운화이트 25000￦<br>▲ 베이컨포테이토골드 25000￦<br>▲ 쉬림프골드 25000￦<br>▲ 고구마치즈스트링 23000￦<br><br><br>◆ 부산광역시 남구 대연3동 40-3<br>◆ 051-612-2330</li>'],
+    [35.13586,129.1025479,'피자마루','<li class="title" style="width:100%;">피자마루</li>'+'<li class="content">▲ 치즈 핵폭탄 피자 19900￦<br>▲ 페퍼로니 치즈폭탄 피자 18900￦<br>▲ 쉬림프 깐풍 피자 14900￦<br>▲ 치폴레 스테이크 피자 14900￦<br>▲ 씨푸드 새게 피자 14900￦<br>▲ 몽빼르 피자 15900￦<br>▲ 크레이지 불새 피자 15900￦<br><br><br>◆ 부산광역시 남구 대연3동 40-3<br>◆ 051-612-2330</li>'],
+    [35.1374704,129.1050029,'반올림피자샵','<li class="title" style="width:100%;">반올림피자샵</li>'+'<li class="content">▲ 오리지널피자R+오븐스파게티 16500￦<br>▲ 오리지널피자L+오븐스파게티 18500￦<br>▲ 오리지널피자L+훈제치킨 24000￦<br>▲ 오리지널피자R+훈제치킨 22000￦<br><br><br>◆ 부산 남구 용소로19번길 88<br>◆ 051-628-3888<br></li>'],
+    [35.1339352,129.1004549,'피자에땅','<li class="title" style="width:100%;">피자에땅</li>'+'<li class="content">▲ 맵고달고피자 22900￦<br>▲ 신세개피자2 19900￦<br>▲마불 피자 18900￦<br>▲ 울트라콤비네이션 18900￦<br>▲ 리얼고구마 18900￦<br>▲ 퐁듀눈꽃치즈피자 21900￦<br><br><br>◆ 부산 남구 용소로40번길 10<br>◆ 051-627-1700</li>'],
+    [35.1352788,129.0991187,'피자브로','<li class="title" style="width:100%;">피자브로</li>'+'<li class="content">▲ 숯불돼지 철판김치브로 18500￦<br>▲ 직화소불고기브로 18500￦<br>▲ 몰디브쉬림프브로 18500￦<br>▲ 스윗고구마피자 13500￦<br>▲ 페페로니피자 13500￦<br>▲ 아이원트피자 14500￦<br><br><br>◆ 부산 남구 용소로14번길 37-1<br>◆ 051-623-8792</li>']
+];
+var markers=[];
+
+function initMap(value1){
+            var map= new google.maps.Map(document.getElementById('map'),{
+            zoom:16,
+            center: new google.maps.LatLng(35.1367833,129.1016051)
+            })
+            alert(typeof value1);
+            if(value1==111)
+                for(var i=0;i<goukbab.length;i++)
+                        setMarker1(map);
+            else if(value1==110)
+                for(var i=0;i<pizza.length;i++)
+                        setMarker2(map);
+}
+
+var i=0;
+
+function setMarker1(map){
+    var korea,marker;
+    
+        korea=goukbab[i];
+        marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position:{lat:korea[0],lng:korea[1]}
+    });
+    markers.push(marker);
+    document.getElementById("info").innerHTML=korea[3];    
+    google.maps.event.addListener(marker,'click',(function(){
+        return function(){     
+            document.getElementById("info").innerHTML=korea[3];
+            toggleBounce(marker);
+        }
+    })(marker,i))
+    i++;
+}
+function setMarker2(map){
+    var korea,marker;
+    
+        korea=pizza[i];
+        marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position:{lat:korea[0],lng:korea[1]}
+    });
+    markers.push(marker);
+    document.getElementById("info").innerHTML=korea[3];    
+    google.maps.event.addListener(marker,'click',(function(){
+        return function(){     
+            document.getElementById("info").innerHTML=korea[3];
+            toggleBounce(marker);
+        }
+    })(marker,i))
+    i++;
+}
+
+function setMapOnAll(map){
+    for(var i=0;i<markers.length;i++){
+        markers[i].setMap(map);
+    }
+}
+function clearMarkers(){
+    setMapOnAll(map);
+}
+function deleteMarker(number){
+    clearMarkers();
+    markers=[];
+}
+
+function toggleBounce(marker) {
+    
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function ()
+    {
+        marker.setAnimation(null);
+        $(marker).dequeue();
+    }, 1400);
+  }
+
+
